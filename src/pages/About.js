30f101bad611c8d75789ebfc3af6d7f7508e7a1d
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-11-04 10:10:23 -0300

    About signature done

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import HeaderContent from '../components/HeaderContent'
 import FooterContent from '../components/FooterContent'
 
 import Model from '../img/model2.png'
+import {ReactComponent as Signature} from '../svg/signature.svg';
 
 const About = () => {
     return (
@@ -16,6 +17,7 @@ const About = () => {
                     <div className='aboutTextWrap'>
                         <h2>About Us</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas feugiat luctus. Duis et sem posuere, hendrerit justo id, ornare massa. Morbi laoreet mauris mauris, quis congue augue accumsan finibus. Maecenas consectetur urna ac mauris dictum, pharetra dictum erat tempor. Suspendisse potenti.</p>
+                        <Signature className='signature'/>
                     </div>
 
                 </div>
