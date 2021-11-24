c665c76bdc6c2ebfa40ef93ec4e398e312ec3b10
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-11-24 16:40:45 -0300

    Arrow down icon set, search bar start

--- src/components/SalesOptions.js
+++ src/components/SalesOptions.js
@@ -7,8 +7,9 @@ import {ReactComponent as ArrowDownBtn} from '../icon/arrowdownbtn.svg';
 const SalesOptions = () => {
     return (
         <div className='saleOptionsWrap'>
+            <input type="text" placeholder="Search.."></input>
             <button>
-                <ArrowDownBtn className='hmbgIcon'/>
+                <ArrowDownBtn className='salesIcon'/>
             </button>            
         </div>
     )
